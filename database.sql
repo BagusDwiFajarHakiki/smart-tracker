926faed8fbcc4127082f3527d7e4d84908b4413c
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2026-01-19 17:18:53 +0700

    feat: Add `shock_val` and `last_fall_time` columns to `tracker_status` table and remove initial data insertion.

--- database.sql
+++ database.sql
@@ -6,17 +6,15 @@ USE smart_tracker;
 DROP TABLE IF EXISTS tracker_logs;
 
 CREATE TABLE IF NOT EXISTS tracker_status (
-    id INT PRIMARY KEY,
-    lat DECIMAL(10, 8),
-    lng DECIMAL(11, 8),
-    battery INT,
-    wifi_status VARCHAR(50),
-    gps_status VARCHAR(50),
-    satellites INT DEFAULT 0,
-    is_fallen BOOLEAN,
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `id` int(11) NOT NULL,
+  `lat` decimal(10,8) DEFAULT NULL,
+  `lng` decimal(11,8) DEFAULT NULL,
+  `battery` int(11) DEFAULT NULL,
+  `wifi_status` varchar(50) DEFAULT NULL,
+  `gps_status` varchar(50) DEFAULT NULL,
+  `satellites` int(11) DEFAULT 0,
+  `is_fallen` tinyint(1) DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `shock_val` int(11) DEFAULT 0,
+  `last_fall_time` datetime DEFAULT NULL
 );
-
--- Initialize with one row (ID=1)
-INSERT IGNORE INTO tracker_status (id, lat, lng, battery, wifi_status, gps_status, satellites, is_fallen) 
-VALUES (1, 0, 0, 0, 'Disconnected', 'Searching', 0, 0);
